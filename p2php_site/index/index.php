c3fb2bbe6b7043bb4b3b91cdbe2908121acc5144
unknown <mitsueyanoo@gmail.com>
2025-05-06 02:31:10 -0300

    Página Inicial
    Adicionei o css padrão geral; Comecei a desenvolver a página inicial

---
+++ p2php_site/index/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>INÍCIO</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <div id="header">
+        <h1>ECOFRAME</h1>
+        <span>Foto: José Aparecido dos Santos</span>
+    </div>
+    <div id="navbar">
+        <a href="../index/index.php" id="selected">INÍCIO</a>
+        <a href="../explorar/explorar.php">EXPLORAR</a>
+        <a href="../cadastro_login/cadastro_login.php">ENTRE / CADASTRE-SE</a>
+    </div>
+    <div id="conteudo">
+        <div id="descricao">
+            <div id="texto">
+                <p id="lema">Descubra, Registre, Proteja.</p>
+                <p>Bem-vindo à nossa plataforma colaborativa de catalogação da biodiversidade! Aqui, você pode registrar e gerenciar dados sobre a fauna e flora local, ajudando a construir um banco de dados rico e dinâmico, acessível a todos.</p>
+                <p>Nossa plataforma permite que especialistas e cidadãos contribuam juntos para a preservação da natureza. Registre suas observações com informações detalhadas, como nome científico e localização, e contribua para o crescimento de um conhecimento compartilhado.</p>
+                <p>Com uma estrutura sólida de usuários, registros biológicos e validação pela comunidade, nossa ferramenta oferece uma maneira eficiente de catalogar, atualizar e corrigir dados em tempo real. Vamos trabalhar juntos pela conservação e pelo futuro da biodiversidade!</p>
+            </div>
+            <div id="dados">
+                <div class="bloco">
+                    <span class="num">258</span> <span>Registros</span>
+                </div>
+                <div class="bloco">
+                    <span class="num">68</span> <span>Espécies</span>
+                </div>
+                <div class="bloco">
+                    <span class="num">112</span> <span>Colaboradores</span>
+                </div>
+            </div>
+        </div>
+        <div id="destaques">
+            <p>Destaques</p>
+        </div>
+    </div>
+</body>
+</html>
